03daf6542d8ad44a66c3cf66d332f59845a6aa8c
GuJie <1045478997@qq.com>
2021-03-05 23:44:18 +0800

    插入100万订单模拟数据，测试不同方式的插入效率
    修改时区

--- Week_07/inserttest/src/test/java/com/gujie/inserttest/InserttestApplicationTests.java
+++ Week_07/inserttest/src/test/java/com/gujie/inserttest/InserttestApplicationTests.java
@@ -1,10 +1,10 @@
-package com.gujie.business;
+package com.gujie.inserttest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BusinessApplicationTests {
+class InserttestApplicationTests {
 
     @Test
     void contextLoads() {
